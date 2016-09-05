790be6229a07430d11442547c4ea96b27153665b
Ian Hom <ianhom@163.com>
2016-09-05 13:46:43 +0800

    Create type.cpp

---
+++ type.cpp
@@ -0,0 +1,25 @@
+#include <iostream>   
+using namespace std;  /* 命名空间 */
+
+int main()
+{
+    cout << "Size of char : " << sizeof(char)  << endl;
+    cout << "Size of int : "  << sizeof(int)   << endl;
+    cout << "Size of short int : " << sizeof(short int) << endl;
+    cout << "Size of long int : "  << sizeof(long int)  << endl;
+    cout << "Size of float : "   << sizeof(float)   << endl;
+    cout << "Size of double : "  << sizeof(double)  << endl;
+    cout << "Size of wchar_t : " << sizeof(wchar_t) << endl;
+    return 0;
+}
+
+/* 运行结果 */
+/*
+Size of char : 1
+Size of int : 4
+Size of short int : 2
+Size of long int : 8
+Size of float : 4
+Size of double : 8
+Size of wchar_t : 4
+*/
